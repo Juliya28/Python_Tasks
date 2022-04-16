232da5e5e6c171b15dd45cb0e5a1219fff8839d2
Juliya <yuliya_2808@mail.ru>
2022-04-16 15:50:57 +0300

    task_5 new

--- task_5.py
+++ task_5.py
@@ -1,7 +1,4 @@
 # 5. Дано число. Проверить кратно ли оно 5 и 10 или 15 но не 30.
 
 num = int(input('Введите число: '))
-if ((num % 5 == 0 and num % 10 == 0) or num % 15 == 0) and num % 30 !=0:
-    print('True')
-else:
-    print('False')
+print (bool(num % 5 == 0 and num % 10 == 0) or num % 15 == 0) and num % 30 !=0
