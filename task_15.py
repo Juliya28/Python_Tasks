7639a5e6e770e0f8434a57c642e957b25a50910a
Juliya <yuliya_2808@mail.ru>
2022-04-24 12:37:43 +0300

    task_15

---
+++ task_15.py
@@ -0,0 +1,20 @@
+# 15. Написать программу, получающую набор произведений чисел от 1 до N.
+# Пример: пусть N = 4, тогда
+# [ 1, 2, 6, 24 ]
+
+import os
+import random
+os.system("cls")
+
+n = random.randint(6, 16)
+print(f'\n {n} \n ')
+
+list = [1]
+
+for i in range(2, n+1):
+    list.append(i * list[i-2])
+
+print(list, '\n')
+
+
+
